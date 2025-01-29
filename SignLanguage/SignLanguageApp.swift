b3c1e70107436b7757d6579f21874cd4a0f0da59
manvithapula <manvithapula@gmail.com>
2025-01-29 17:36:34 +0530

    Initial Commit

---
+++ SignLanguage/SignLanguageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignLanguageApp.swift
+//  SignLanguage
+//
+//  Created by admin64 on 29/01/25.
+//
+
+import SwiftUI
+
+@main
+struct SignLanguageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SignLanguageTests/SignLanguageTests.swift
@@ -0,0 +1,17 @@
+//
+//  SignLanguageTests.swift
+//  SignLanguageTests
+//
+//  Created by admin64 on 29/01/25.
+//
+
+import Testing
+@testable import SignLanguage
+
+struct SignLanguageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
